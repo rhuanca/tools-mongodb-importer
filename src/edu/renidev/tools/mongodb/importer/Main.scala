f95a8562d6f9f9e0250df1f143f44e09034d125c
Renan Huanca <rhuanca@gmail.com>
2014-11-14 22:01:25 -0500

    added worker

--- src/edu/renidev/tools/mongodb/importer/Main.scala
+++ src/edu/renidev/tools/mongodb/importer/Main.scala
@@ -1,12 +1,48 @@
 package edu.renidev.tools.mongodb.importer
 
-import scala.swing._
+import java.io.File
+import java.net.URL
+
+import scala.swing.Dimension
+import scala.swing.MainFrame
+import scala.swing.SimpleSwingApplication
+import scala.sys.process.urlToProcess
 
 object Main extends SimpleSwingApplication {
   def top = new MainFrame {
     title = "Mongo DB Collections Importer"
     preferredSize = new Dimension(400, 150)
-    contents = new MainPanel()
+    val form = new MainPanel();
+    contents = form
     peer.setLocationRelativeTo(null)
+    listenTo(form)
+    reactions += {
+      case ImportEvent =>
+        {
+          val worker = new Thread(new Worker(form.getUrl(), "output.txt", form))
+          worker.start()
+          //new URL(form.getUrl()) #> new File("output.txt") !
+
+          //        val in = scala.io.Source.fromURL(form.getUrl(),
+          //          "utf-8")
+          //        
+          //        for (line <- in.getLines)
+          //          println(line)
+        }
+    }
+  }
+
+  class Worker(val initialUrl: String, val initialOutput:String, val initialForm: MainPanel) extends Runnable {
+    private var url = initialUrl;
+    private var out = initialOutput;
+    private var form = initialForm;
+    def run {
+      form.setEnabled(false)
+      form.updateStatus("downloading json")
+      
+      new URL(url) #> new File(out) !
+
+      form.setEnabled(true)
+    }
   }
 }
--- src/edu/renidev/tools/mongodb/importer/MainPanel.scala
+++ src/edu/renidev/tools/mongodb/importer/MainPanel.scala
@@ -8,40 +8,64 @@ import scala.swing.Orientation
 import scala.swing.Swing
 import scala.swing.TextField
 import scala.swing.event.ButtonClicked
+import scala.swing.event.Event
 
-import javax.swing.JOptionPane
+case object ImportEvent extends Event
 
 class MainPanel extends BorderPanel {
 
-  object url extends TextField("http://107.170.110.155:3121/games/2014102300/entities.json") { columns = 50 }
+  object url extends TextField("http://107.170.110.155:3121/games/2014102300/entities") { columns = 50 }
   object collectionName extends TextField { columns = 50 }
+  object status extends Label
   object host extends TextField("localhost:27017") { columns = 50 }
   object ok extends Button("Import")
-  
-  
-  
-  def form = new BoxPanel(Orientation.Horizontal) {
-    contents += new BoxPanel(Orientation.Vertical) {
+
+  def getUrl() = url.text
+
+  def updateStatus(statusParameter: String) {
+    status.text = statusParameter
+  }
+
+  def setEnabled(enabled: Boolean) {
+    url.enabled = enabled
+    collectionName.enabled = enabled
+    host.enabled = enabled
+    status.enabled = enabled
+    ok.enabled = enabled
+  }
+
+  def form = new BoxPanel(Orientation.Vertical) {
+    contents += new BoxPanel(Orientation.Horizontal) {
       contents += new Label("URL: ")
-      contents += new Label("Collection Name: ")
-      contents += new Label("Host: ")
-    }
-    contents += new BoxPanel(Orientation.Vertical) {
       contents += url
+    }
+
+    contents += new BoxPanel(Orientation.Horizontal) {
+      contents += new Label("Collection Name: ")
       contents += collectionName
+    }
+
+    contents += new BoxPanel(Orientation.Horizontal) {
+      contents += new Label("Host: ")
       contents += host
+    }
+
+    contents += new BoxPanel(Orientation.Horizontal) {
+      contents += new Label("Status: ")
+      contents += status
     }
   }
 
   border = Swing.EmptyBorder(5, 5, 5, 5)
-  add(form, BorderPanel.Position.North)
-  add(ok, BorderPanel.Position.South)
+
+  layout(form) = BorderPanel.Position.North
+  layout(ok) = BorderPanel.Position.South
 
   listenTo(ok)
 
   reactions += {
-    case ButtonClicked(component) if component == ok =>
-      JOptionPane.showMessageDialog(null, "hello")
-      
+    case ButtonClicked(component) =>  { 
+      publish(ImportEvent)
+    }
   }
 }
